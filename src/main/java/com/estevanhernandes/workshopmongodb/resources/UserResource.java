60fd04852de55445273114738b75f0b2b45475f2
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2023-08-11 19:28:18 -0300

    getting user by id

--- src/main/java/com/estevanhernandes/workshopmongodb/resources/UserResource.java
+++ src/main/java/com/estevanhernandes/workshopmongodb/resources/UserResource.java
@@ -7,6 +7,7 @@ import org.apache.coyote.Response;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -25,5 +26,12 @@ public class UserResource {
         List<UserDTO> listDTO = usersList.stream().map(UserDTO::new).toList();
 
         return ResponseEntity.ok().body(listDTO);
+    }
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<UserDTO> findById(@PathVariable String id) {
+        User user = service.findById(id);
+
+        return ResponseEntity.ok().body(new UserDTO(user));
     }
 }
--- src/main/java/com/estevanhernandes/workshopmongodb/services/UserService.java
+++ src/main/java/com/estevanhernandes/workshopmongodb/services/UserService.java
@@ -2,10 +2,12 @@ package com.estevanhernandes.workshopmongodb.services;
 
 import com.estevanhernandes.workshopmongodb.domain.User;
 import com.estevanhernandes.workshopmongodb.repositories.UserRepository;
+import com.estevanhernandes.workshopmongodb.services.exceptions.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -15,5 +17,11 @@ public class UserService {
 
     public List<User> findAll() {
         return repository.findAll();
+    }
+
+    public User findById(String id) {
+        Optional<User> user = repository.findById(id);
+
+        return user.orElseThrow(() -> new ObjectNotFoundException("User not found"));
     }
 }
