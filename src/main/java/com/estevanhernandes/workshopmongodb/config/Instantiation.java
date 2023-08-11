3dad95f43bc4f386bb8c3cf01325ac133413245c
Estevan Hernandes Silva de Oliveira <estevanhernandes1@gmail.com>
2023-08-11 20:49:15 -0300

    adding comments to posts

--- src/main/java/com/estevanhernandes/workshopmongodb/config/Instantiation.java
+++ src/main/java/com/estevanhernandes/workshopmongodb/config/Instantiation.java
@@ -3,6 +3,7 @@ package com.estevanhernandes.workshopmongodb.config;
 import com.estevanhernandes.workshopmongodb.domain.Post;
 import com.estevanhernandes.workshopmongodb.domain.User;
 import com.estevanhernandes.workshopmongodb.dto.AuthorDTO;
+import com.estevanhernandes.workshopmongodb.dto.CommentDTO;
 import com.estevanhernandes.workshopmongodb.repositories.PostRepository;
 import com.estevanhernandes.workshopmongodb.repositories.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -25,6 +26,7 @@ public class Instantiation implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         userRepository.deleteAll();
+        postRepository.deleteAll();
 
         User maria = new User(null, "Maria Brown", "maria@gmail.com");
         User alex = new User(null, "Alex Green", "alex@gmail.com");
@@ -34,6 +36,13 @@ public class Instantiation implements CommandLineRunner {
 
         Post post1 = new Post(null, Instant.parse("2018-03-21T19:53:07Z"), "Partiu viagem", "\"Vou viajar para São Paulo. Abraços!", new AuthorDTO((maria)));
         Post post2 = new Post(null, Instant.parse("2018-03-23T09:30:59Z"), "Bom dia", "Acordei feliz hoje!", new AuthorDTO((maria)));
+
+        CommentDTO c1 = new CommentDTO("Boa viagem mano!", Instant.parse("2018-03-21T20:00:07Z"), new AuthorDTO(alex));
+        CommentDTO c2 = new CommentDTO("Aproveite!", Instant.parse("2018-03-21T21:00:07Z"), new AuthorDTO(bob));
+        CommentDTO c3 = new CommentDTO("Tenha um ótimo dia!",Instant.parse("2018-03-23T09:45:59Z"), new AuthorDTO(alex));
+
+        post1.getComments().addAll(Arrays.asList(c1, c2));
+        post2.getComments().add(c3);
 
         postRepository.saveAll(Arrays.asList(post1, post2));
 
--- src/main/java/com/estevanhernandes/workshopmongodb/domain/Post.java
+++ src/main/java/com/estevanhernandes/workshopmongodb/domain/Post.java
@@ -1,11 +1,14 @@
 package com.estevanhernandes.workshopmongodb.domain;
 
 import com.estevanhernandes.workshopmongodb.dto.AuthorDTO;
+import com.estevanhernandes.workshopmongodb.dto.CommentDTO;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Objects;
 
 @Document
@@ -16,6 +19,7 @@ public class Post implements Serializable {
     private String title;
     private String body;
     private AuthorDTO author;
+    private List<CommentDTO> comments = new ArrayList<>();
 
     public Post() { }
 
@@ -62,6 +66,10 @@ public class Post implements Serializable {
     public AuthorDTO getAuthor() {return author; }
 
     public void setAuthor(AuthorDTO author) { this.author = author; }
+
+    public List<CommentDTO> getComments() { return comments; }
+
+    public void setComments(List<CommentDTO> comments) { this.comments = comments; }
 
     @Override
     public boolean equals(Object o) {
